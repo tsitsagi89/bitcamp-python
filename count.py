4daf88b9aaa226d526bce8d3724c66bfd7d61e2c
nikolozGobejiShvili <123975638+nikolozGobejiShvili@users.noreply.github.com>
2023-03-13 17:31:30 +0400

    Add files via upload

---
+++ count.py
@@ -0,0 +1,17 @@
+
+def main():
+
+    text = input("What is the input string?")
+    if text !="":
+    
+
+      print( text +  " has " + str(len(text)) + " character")
+    else:
+       print("input string")  
+    
+
+
+
+
+main()
+
