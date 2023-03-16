157deb9bb87992d3b124ec33b4f403e968c15ea8
tsitsagi89 <99443438+tsitsagi89@users.noreply.github.com>
2023-03-17 00:46:32 +0400

    Update coke.py

--- coke.py
+++ coke.py
@@ -1,26 +1,14 @@
-i=50
-print("amount due " + str(i))
-while True:
-    coin=int(input("insert coin "))
 
-    if coin== 5 or coin==10 or coin==25:
-        i-=coin
+i = 0
+while i < 50:
+    coin = int(input("insert coin: "))
+    if coin == 10 or coin == 5 or coin == 25:
+         i = i + coin
+    else:
+        i = i + 0
+    if i < 50:
+        print("Amount Due: ", 50 - i)
+    else:
+        break
 
-        if i > 0:
-            print("amount due " + str(i))
-
-            continue
-        elif i == 0:
-            print("Change Owed " + str(i) )
-            
-            break
-        elif i < 0:
-            print("Change Owed " + str(i * -1)) 
-
-            break   
-
-                         
-         
-
-    
-        
+print(f"Change Owed: {i - 50}")    
